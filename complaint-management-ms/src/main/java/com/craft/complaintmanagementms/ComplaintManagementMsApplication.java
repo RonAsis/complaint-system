ae72b7daa712ed33a78a58e3a99fbfb936ad8a59
rasis <ron.asis@rbbn.com>
2022-03-07 22:45:34 +0200

    Added complaint-management-ms

---
+++ complaint-management-ms/src/main/java/com/craft/complaintmanagementms/ComplaintManagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.craft.complaintmanagementms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintManagementMsApplication.class, args);
+	}
+
+}
---
+++ complaint-management-ms/src/test/java/com/craft/complaintmanagementms/ComplaintManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craft.complaintmanagementms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
